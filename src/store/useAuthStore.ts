971ca969b80db059de68de45f7f60ad398033cc5
ashmit <ashmitbastola@gmail.com>
2025-04-15 20:18:39 +0545

    env updated

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL="http://localhost:5001"
+const BASE_URL=import.meta.env.VITE_BASE_URL! 
 
 type authTypes={
   authUser:{_id:string,profilePic:string,fullName:string,email:string,createdAt:string} | null
